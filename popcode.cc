d11d07409f7f4a4d832a43bc7cdcd57475d6c1e9
paolosyloslabini <44007707+paolosyloslabini@users.noreply.github.com>
2022-06-07 10:43:05 +0200

    Create popcode.cc

---
+++ popcode.cc
@@ -0,0 +1,32 @@
+#include <poplar/DeviceManager.hpp>
+#include <poplar/Engine.hpp>
+#include <poplar/Graph.hpp>
+
+#include <algorithm>
+#include <iostream>
+
+using namespace poplar;
+
+int main() {
+  // Create the DeviceManager which is used to discover devices
+  auto manager = DeviceManager::createDeviceManager();
+
+  // Attempt to attach to a single IPU:
+  auto devices = manager.getDevices(poplar::TargetType::IPU, 1);
+  std::cout << "Trying to attach to IPU\n";
+  auto it = std::find_if(devices.begin(), devices.end(), [](Device &device) {
+     return device.attach();
+  });
+ if (it == devices.end()) {
+    std::cerr << "Error attaching to device\n";
+    return 1; //EXIT_FAILURE
+  }
+
+  auto device = std::move(*it);
+  std::cout << "Attached to IPU " << device.getId() << std::endl;
+
+  Target target = device.getTarget();
+
+  // Create the Graph object
+  Graph graph(target);
+}
